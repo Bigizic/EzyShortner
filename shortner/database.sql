b109f5d21101c4dd65d0c4f4a6fc44ff0e7fe707
Bigiizc <olalekanisaac75@gmail.com>
2023-10-05 12:47:31 +0100

    Update shortner

--- shortner/database.sql
+++ shortner/database.sql
@@ -1,9 +1,3 @@
 -- prepares a database to store the url records
 
 CREATE DATABASE IF NOT EXISTS Ezy_url;
-USE Ezy_url;
-CREATE TABLE IF NOT EXISTS records (
-	Id INT PRIMARY KEY,
-	Original_url VARCHAR(500) NOT NULL,
-	short_url VARCHAR(10) NOT NULL
-);
