adb443985ebf8c5990a091d0d80aba8e8098f37b
Bigiizc <olalekanisaac75@gmail.com>
2023-10-26 23:24:31 +0100

    Update api

--- api/ezy_v1/app.py
+++ api/ezy_v1/app.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ EZYURL API configuration using Flask and flasgger """
-=======
-""" Flask """
->>>>>>> main
 
 
 from api.ezy_v1.views import app_views
 from flask_cors import CORS
-<<<<<<< HEAD
 from flasgger import Swagger, LazyString, LazyJSONEncoder
-=======
-from flasgger import Swagger
->>>>>>> main
 from flasgger.utils import swag_from
 from flask import Flask, render_template, make_response, jsonify
 from models import storage_type
@@ -20,7 +12,6 @@ from werkzeug.exceptions import NotFound
 
 
 app = Flask(__name__)
-<<<<<<< HEAD
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  # for better readability
 app.json_encoder = LazyJSONEncoder
 app.register_blueprint(app_views)
@@ -28,11 +19,9 @@ app.url_map.strict_slashes = False
 CORS(app, resources={r"/api/ezy_v1/*": {"origins": "*"}})
 
 template = dict(swaggerUiPrefix=LazyString(lambda : request.environ.get('HTTP_X_SCRIPT_NAME', '')))
-=======
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
->>>>>>> main
 
 
 @app.teardown_appcontext
@@ -50,7 +39,6 @@ def not_found(error):
     """
     return make_response(jsonify({"error": "Not found"}), 404)
 
-<<<<<<< HEAD
 app.config['SWAGGER'] = {
     'title': 'Ezy Url Restful API',
     'uiversion': 3
@@ -58,8 +46,6 @@ app.config['SWAGGER'] = {
 
 Swagger(app, template=template)
 
-=======
->>>>>>> main
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, threaded=True)
