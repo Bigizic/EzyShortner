f2b74c783fc0f909dc200cba8ff2fb9ca79d7a0a
Bigiizc <olalekanisaac75@gmail.com>
2023-10-29 23:06:59 +0000

    Update app

--- app/static/scripts/homepage.js
+++ app/static/scripts/homepage.js
@@ -19,4 +19,39 @@ $(document).ready(() => {
   $('#cancelButton').click(function () {
     $('#qr_container').slideUp();
   });
-});
+  $('header h1').click(function () {
+    window.location.replace(window.location.href);
+  });
+
+  $('.menu-button').click(function () {
+    $('.cover-up').css('width', '100%');
+    
+  });
+  
+  $('.close-button').click(function () {
+    $('.cover-up').css('width', '0');
+  });
+
+  /* This sets the alias button */
+  $('.alias').one('click', function () {
+    if ($('#user_output').val()) {
+      $('#user_output').val('');
+    }
+    const currentWidth = parseInt($('#user_output').css('width'));
+    const lPadd = parseInt($('#user_output').css('padding-left'));
+    const rPadd = parseInt($('#user_output').css('padding-right'));
+    const newWidth =  currentWidth - (lPadd + rPadd);
+    $('#user_output').css('width', newWidth);
+    $('#user_output').attr('autofocus', 'autofocus');
+    $('#user_output').removeAttr('readonly');
+    $('#user_output').attr('placeholder', 'Enter an alias e.g JohnDoe');
+    $('.label_user_output').css('color', 'aliceblue');
+    $('.label_user_output').html('www.ezyurl.tech/');
+    $('#user_output').on('input', function () {
+      var userInput = $(this).val();
+      $('.label_user_output').text('www.ezyurl.tech/' + userInput);
+    });
+  });
+  /* end alias button */
+})
+
--- app/web_app.py
+++ app/web_app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, render_template
 from models.ezy import Ezy
 from models.engine.db_storage import DBStorage
 from shortner.qr_img_gen import qr_gen
+import uuid
+import logging
 
 
 app = Flask(__name__)
@@ -15,19 +17,40 @@ app = Flask(__name__)
 def get_input():
     if request.method == "POST":
         user_input = request.form.get("user_input")
-        ezy_instance = Ezy(original_url=user_input)
-        ezy_instance.exists()  # check for existence before saving
-        ezy_instance.save()
-        word = "Long link enterd is not a valid address"
-        short_url = ezy_instance.url() if ezy_instance.url() else word
-        status_code = 200 if short_url != word else 404
+        user_output = request.form.get("user_output")
 
-        qr_file_path = qr_gen(short_url) if short_url != word else ''
+        """ezy_instance = Ezy(original_url=user_input, short_url=user_output) if
+                user_output else Ezy(original_url=user_input))"""
+        ezy_instance = Ezy()
+        ezy_instance.original_url=user_input                            
+        if user_output:
+            ezy_instance.short_url=user_output
+
+        alias = (ezy_instance.exists(user_output) if user_output else
+                ezy_instance.exists())
+
+        # app.logger.warning(alias)
+
+        # ezy_instance.exists()  # check for existence before saving
+        ezy_instance.save()
+
+        wor = "Long link enterd is not a valid address"
+
+        short_url = ezy_instance.url() if ezy_instance.url() and not alias else ''
+        word = wor if not short_url and not alias else ''
+        status_code = 200 if len(short_url) > 1 and not alias else 404
+
+        qr_file_path = qr_gen(short_url) if len(
+                    short_url) > 1 and not alias else ''
+        if not alias:
+            alias = ''
+
 
         return render_template('homepage.html', url=short_url,
-                               status=status_code, qr_image=qr_file_path)
+                               status=status_code, qr_image=qr_file_path,
+                               alias_status=alias, word=word)
 
-    return render_template('homepage.html')
+    return render_template('homepage.html', cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
