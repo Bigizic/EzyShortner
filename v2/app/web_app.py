ce66390d2b7050126d3a7f7a04a6f255849f794a
Bigizic <olalekanisaac75@gmail.com>
2023-11-24 08:30:12 +0000

    Update web_app.py

--- v2/app/web_app.py
+++ v2/app/web_app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/pytohn3
 """Web app using flask"""
 
-
 from flask import Flask, request, render_template, make_response, session
 from models.ezy import Ezy
 from models.users import User
@@ -13,17 +12,18 @@ from app.web_app_routes import web_app_blueprint
 import logging
 
 
+# app = Flask(__name__, static_folder='static', static_url_path='/static')
 app = Flask(__name__)
 app.secret_key = environ.get("FLASK_KEY")
-Logged_in = False
+app.url_map.strict_slashes = False
 
 if not app.debug:
     app.logger.setLevel(logging.INFO)
 
 
-# Initialize logged_in status in session
 @app.before_request
 def before_request():
+    """ Initialize logged_in status in session """
     session.setdefault('logged_in', False)
 
 app.register_blueprint(web_app_blueprint)
