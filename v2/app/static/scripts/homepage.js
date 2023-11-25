9de2c704a398e1d6b479758f2cb26b76bc075318
Bigizic <olalekanisaac75@gmail.com>
2023-11-25 12:01:24 +0000

    Update scripts

--- v2/app/static/scripts/homepage.js
+++ v2/app/static/scripts/homepage.js
@@ -81,25 +81,43 @@ $(document).ready(() => {
   /* end prefered characters */
 
   /* This sets the alias button */
-  $('.alias').one('click', function () {
+  let isAliasActive = false;
+  
+  $('.alias').on('click', function () {
     if ($('#user_output').val()) {
       $('#user_output').val('');
     }
+    
     const currentWidth = parseInt($('#user_output').css('width'));
     const lPadd = parseInt($('#user_output').css('padding-left'));
     const rPadd = parseInt($('#user_output').css('padding-right'));
     const newWidth = currentWidth - (lPadd + rPadd);
-    $('#user_output').css('width', newWidth);
-    $('#user_output').attr('autofocus', 'autofocus');
-    $('#user_output').removeAttr('readonly');
-    $('#user_output').attr('placeholder', 'Enter an alias e.g JohnDoe');
-    $('.label_user_output').css('color', 'aliceblue');
-    $('.label_user_output').css('font-size', 'medium');
-    $('.label_user_output').html(BASEURL + '/');
-    $('#user_output').on('input', function () {
-      const userInput = $(this).val();
-      $('.label_user_output').text(`${BASEURL}/` + userInput);
-    });
+    if (!isAliasActive) {
+      $('#user_output').css('width', newWidth);
+      $('#user_output').attr('autofocus', 'autofocus');
+      $('#user_output').removeAttr('readonly');
+      $('#user_output').attr('placeholder', 'Enter an alias e.g JohnDoe');
+      $('.label_user_output').css('color', 'aliceblue');
+      $('.label_user_output').css('font-size', 'medium');
+      $('.label_user_output').html(BASEURL + '/');
+      $('#user_output').on('input', function () {
+        const userInput = $(this).val();
+        $('.label_user_output').text(`${BASEURL}/` + userInput);
+      });
+      isAliasActive = true;
+    } else {
+      const formerWidth = currentWidth + (lPadd + rPadd);
+      $('#user_output').val('');
+      $('#user_output').css('width', formerWidth);
+      $('#user_output').attr('readonly', 'readonly');
+      $('#user_output').attr('placeholder', '');
+      $('.label_user_output').css('color', '');
+      $('.label_user_output').css('font-size', '');
+      $('.label_user_output').html('');
+      $('#user_output').off('input');
+
+      isAliasActive = false;
+    }
   });
   /* end alias button */
 
