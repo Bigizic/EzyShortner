153ef23225b2f9d62144b5c04a11fa565e1df053
Bigizic <olalekanisaac75@gmail.com>
2023-11-26 03:05:35 +0000

    Delete v2 directory

--- v2/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from models.engine.db_storage import DBStorage
-# Creates an instance of the database model and connects to
-# database module using the storage_type variable
-
-
-storage_type = DBStorage()
-storage_type.reload()
