8b64200d848a85890d3cb1e5fe835f8758379327
Bigizic <olalekanisaac75@gmail.com>
2023-12-02 13:30:06 +0000

    Update web_app_routes.py

--- v2/app/web_app_routes.py
+++ v2/app/web_app_routes.py
@@ -9,9 +9,9 @@ from models.users import User
 from models.engine.db_storage import DBStorage
 from os import environ
 from shortner.qr_img_gen import qr_gen
+import re
 import uuid
 import logging
-
 
 web_app_blueprint = Blueprint('web_app', __name__)
 
@@ -24,106 +24,10 @@ def get_input():
 
     if request.method == "POST":
         user_input = request.form.get("user_input")
-        if len(user_input) >= 32000:
-            return homepage('', '', 404,
-                            'Link must not be greater than 32000', '')
-
-        if user_input.startswith(('https://ezyurl.xyz/', 'ezyurl.xyz/',
-                                  'http://ezyurl.xyz/',
-                                  'https://www.ezyurl.xyz/',
-                                  'http://www.ezyurl.xyz/')):
-            return homepage('', '', 404, '', 'Cannot shorten Ezy Domain')
         user_output = request.form.get("user_output")
-
-        wor = "Long link enterd is not a valid address"
-        alias_error = "Alias has been used please try another"
-
-        ezy_instance = Ezy()
-        ezy_instance.original_url = user_input
-
-        if user_output:
-            bad_alias = ["api", "api/", "api//", "api-docs", "api/docs/",
-                         "api_docs", "signup", "signup/docs", "sign_up",
-                         "sign-up", "signin", "signin/docs", "sign_in",
-                         "sign-in", "aboutus", "about", "about_us",
-                         "about-us", "dashboard", "dash_board",
-                         "dash-board", "dashboard/", "dashboard//",
-                         "dashboard_"]
-
-            if user_output in bad_alias or len(user_output) > 70:
-                return homepage('', '', 404, '', 'Oops... Not Allowd')
-
-            ezy_instance.short_url = user_output
-
-            alias = func_alias(ezy_instance, user_output)
-            if alias == "alias exist original url valid":
-                """This would check if the alias exists before saving
-                and if the original url entered by user is valid"""
-                # ezy_instance.remove_url()  # deletes the created instance
-                ezy_instance.save()
-                return homepage('', 404, '', alias_error, '')
-
-            ezy_instance.save()
-            if alias == "alias exist original url invalid":
-                ezy_instance.remove_url()  # deletes the created instance
-                return homepage('', 404, '', '', wor)
-            if alias == "alias doesn't exist original url invalid":
-                ezy_instance.remove_url()  # deletes the created instance
-                return homepage('', 404, '', '', wor)
-            if alias == "alias doesn't exist original url valid":
-                short_url = ezy_instance.url()
-                qr_file_path = qr_gen(short_url)
-                return homepage('https://' + short_url, 200,
-                                qr_file_path, '', '')
-        else:
-            ezy_instance.exists()  # check for existence before saving
-            ezy_instance.save()
-            if ezy_instance.url():
-                short_url = ezy_instance.url()
-                qr_file_path = qr_gen(short_url)
-                return homepage('https://' + short_url, 200,
-                                qr_file_path, '', '')
-            else:
-                return homepage('', 404, '', '', wor)
+        res = application(user_input, user_output)
+        return homepage(res[0], res[1], res[2], res[3], res[4])
     return render_template('homepage.html', cache_id=uuid.uuid4())
-
-
-def homepage(short_url, status_code, qr_file_path, alias, word):
-    """Renders homepage"""
-    return render_template('homepage.html', url=short_url,
-                           status=status_code, qr_image=qr_file_path,
-                           alias_status=alias, word=word,
-                           cache_id=uuid.uuid4())
-
-
-def func_alias(ezy_instance, user_output):
-    """Handles:
-    ALIAS EXIST ORIGINAL URL INVALID
-                |
-    Return: (str) "alias exist original url invalid"
-    ALIAS EXIST ORIGINAL URL VALID
-                |
-    Return: (str) "alias exist original url valid"
-
-    ALIAS DOESN'T EXIST ORIGINAL URL INVALID
-                |
-    Return: (str) "alias doesn't exist original url invalid"
-    ALIAS DOESN'T EXIST ORIGINAL URL VALID
-                |
-    Return: (str) "alias doesn't exist original url valid"
-    """
-    alias = ezy_instance.exists(user_output)
-    valid_url = ezy_instance.url()
-
-    if alias and not valid_url:
-        return "alias exist original url invalid"
-    if alias and valid_url:
-        return "alias exist original url valid"
-
-    if not alias and not valid_url:
-        return "alias doesn't exist original url invalid"
-    if not alias and valid_url:
-        return "alias doesn't exist original url valid"
 
 
 @web_app_blueprint.route('/about')
@@ -217,70 +121,12 @@ def sign_in():
 @web_app_blueprint.route('/dashboard/<user_id>', methods=["GET", "POST"],
                          strict_slashes=False)
 def dashboard(user_id):
+    """User's dashboard"""
     if request.method == "POST":
         user_input = request.form.get("user_input")
-        if len(user_input) >= 32000:
-            return dashpage('', '', 404,
-                            'Link must not be greater than 32000', '')
-
-        if user_input.startswith(('https://ezyurl.xyz/', 'ezyurl.xyz/',
-                                  'http://ezyurl.xyz/',
-                                  'https://www.ezyurl.xyz/',
-                                  'http://www.ezyurl.xyz/')):
-            return dashpage('', '', 404, '', 'Cannot shorten Ezy Domains')
         user_output = request.form.get("user_output")
-
-        wor = "Long link enterd is not a valid address"
-        alias_error = "Alias has been used please try another"
-
-        ezy_instance = Ezy()
-        ezy_instance.original_url = user_input
-        ezy_instance.user_id = user_id
-
-        if user_output:
-            bad_alias = ["api", "api/", "api//", "api-docs", "api/docs/",
-                         "api_docs", "signup", "signup/docs", "sign_up",
-                         "sign-up", "signin", "signin/docs", "sign_in",
-                         "sign-in", "aboutus", "about", "about_us",
-                         "about-us", "dashboard", "dash_board",
-                         "dash-board", "dashboard/", "dashboard//",
-                         "dashboard_"]
-
-            if user_output in bad_alias or len(user_output) > 70:
-                return dashpage('', '', 404, '', 'Oops... Not Allowd')
-
-            ezy_instance.short_url = user_output
-
-            alias = func_alias(ezy_instance, user_output)
-            if alias == "alias exist original url valid":
-                """This would check if the alias exists before saving
-                and if the original url entered by user is valid"""
-                # ezy_instance.remove_url()  # deletes the created instance
-                ezy_instance.save()
-                return dashpage('', 404, '', alias_error, '')
-
-            ezy_instance.save()
-            if alias == "alias exist original url invalid":
-                ezy_instance.remove_url()  # deletes the created instance
-                return dashpage('', 404, '', '', wor)
-            if alias == "alias doesn't exist original url invalid":
-                ezy_instance.remove_url()  # deletes the created instance
-                return dashpage('', 404, '', '', wor)
-            if alias == "alias doesn't exist original url valid":
-                short_url = ezy_instance.url()
-                qr_file_path = qr_gen(short_url)
-                return dashpage('https://' + short_url, 200,
-                                qr_file_path, '', '')
-        else:
-            ezy_instance.exists()  # check for existence before saving
-            ezy_instance.save()
-            if ezy_instance.url():
-                short_url = ezy_instance.url()
-                qr_file_path = qr_gen(short_url)
-                return dashpage('https://' + short_url, 200,
-                                qr_file_path, '', '')
-            else:
-                return dashpage('', 404, '', '', wor)
+        res = application(user_input, user_output, user_id)
+        return dashpage(res[0], res[1], res[2], res[3], res[4])
 
     if ('logged_in' in session and session['logged_in'] and
             session['user_id'] == user_id):
@@ -303,21 +149,6 @@ def dashboard(user_id):
         return redirect(url_for('web_app.sign_in'))
 
 
-def dashpage(short_url, status_code, qr_file_path, alias, word):
-    """Renders dashpage"""
-    user = User().exists(None, None, session.get('user_id'))
-    if user:
-        info = DBStorage().fetch_user(session.get('user_id'))
-        names = info.first_name + ' ' + info.last_name
-        email = info.email[:2].upper()
-
-    return render_template('dashboard.html', url=short_url,
-                           status=status_code, qr_image=qr_file_path,
-                           alias_status=alias, word=word,
-                           cache_id=uuid.uuid4(), user_id=session['user_id'],
-                           names=names, email=email)
-
-
 @web_app_blueprint.route('/dashboard', methods=["GET", "POST"])
 def dashboard_helper():
     """Incase a user enters a route like server_name/dashboard"""
@@ -338,60 +169,10 @@ def history(user_id):
 
     if ('logged_in' in session and session['logged_in'] and
             session['user_id'] == user_id):
-
         return historypage(user_id)
+
     else:
         session['info_message'] = "Sign in to continue"
-        return redirect(url_for('web_app.sign_in'))
-
-
-def historypage(user_id, query=None):
-    """Renders the history page returns crucial info"""
-    if query:
-        long_result = DBStorage().search(user_id, query)
-        current_app.logger.warning(query)
-        short_result = DBStorage().search(user_id, None,
-                                          query.split('/')[2])
-        current_app.logger.warning(short_result)
-
-        result = long_result if long_result else short_result
-        search_result = []
-
-        current_app.logger.warning(result)
-        user = User().exists(None, None, user_id)
-        info = DBStorage().fetch_user(user_id)
-        names = info.first_name + ' ' + info.last_name
-        email = info.email[:2].upper()
-
-        for obj in result:
-            cl_name = type(obj).__name__
-            id = obj.id
-            attr = {k: v for k, v in obj.__dict__.items()
-                    if not k.startswith('_sa_instance_state')}
-            
-            search_result.append({
-                **attr
-            })
-        return render_template('user_routes/history.html',
-                               cache_id=uuid.uuid4(),
-                               email=email, names=names,
-                               user_id=user_id,
-                               history_items=search_result)
-
-    user = User().exists(None, None, user_id)
-    if user:
-        info = DBStorage().fetch_user(user_id)
-        names = info.first_name + ' ' + info.last_name
-        email = info.email[:2].upper()
-        history = DBStorage().fetch_user_and_ezy(user_id)
-        return render_template('user_routes/history.html',
-                               cache_id=uuid.uuid4(),
-                               email=email, names=names,
-                               user_id=user_id,
-                               history_items=history)
-    else:
-        # direct a user if they've been blocked to sign in
-        session['info_message'] = "Account doesn't exist"
         return redirect(url_for('web_app.sign_in'))
 
 
@@ -424,6 +205,176 @@ def delete_history(ezy_url_id):
         session['info_message'] = "Sign in to continue"
         return redirect(url_for('web_app.sign_in'))
 
+"""FUNCTIONS
+"""
+
+def application(user_input, user_output, user_id=None):
+    if len(user_input) >= 32000:
+        return '', '', 404, 'Link must not be greater than 32000', ''
+
+    if user_input.startswith(('https://ezyurl.xyz/', 'ezyurl.xyz/',
+                              'http://ezyurl.xyz/',
+                              'https://www.ezyurl.xyz/',
+                              'http://www.ezyurl.xyz/')):
+        return '', '', 404, '', 'Cannot shorten Ezy Domain'
+
+    wor = "Long link enterd is not a valid address"
+    alias_error = "Alias has been used please try another"
+
+    ezy_instance = Ezy()
+    ezy_instance.original_url = user_input
+
+    if user_id:
+        ezy_instance.user_id = user_id
+
+    if user_output:
+        bad_alias = ["api", "api-docs", "api_docs", "signup", "sign_up",
+                     "sign-up", "signin", "sign_in", "sign-in", "aboutus",
+                     "about", "about_us","about-us", "dashboard",
+                     "dash_board", "dash-board", "dashboard_", "ezy", "ezy.",
+                     "ezy.com"]
+        pattern = r'[/\\]'
+        sec = r'\.\w+'
+
+        if user_output in bad_alias or len(user_output) > 70:
+            return '', '', 404, '', 'Oops... Not Allowd'
+
+        ezy_instance.short_url = user_output
+        alias = func_alias(ezy_instance, user_output)
+        if alias == "alias exist original url valid":
+            ezy_instance.remove_url()  # deletes the created instance
+            return '', 404, '', alias_error, ''
+        if alias == "alias exist original url invalid":
+            ezy_instance.remove_url()  # deletes the created instance
+            return '', 404, '', '', wor
+
+        if alias == "alias doesn't exist original url invalid":
+            ezy_instance.remove_url()  # deletes the created instance
+            return '', 404, '', '', wor
+        if alias == "alias doesn't exist original url valid":
+            ezy_instance.save()
+            short_url = ezy_instance.url()
+            qr_file_path = qr_gen(short_url)
+            return 'https://' + short_url, 200, qr_file_path, '', ''
+    else:
+        if ezy_instance.url():
+            ezy_instance.exists()  # check for existence before saving
+            ezy_instance.save()
+            short_url = ezy_instance.url()
+            qr_file_path = qr_gen(short_url)
+            return 'https://' + short_url, 200, qr_file_path, '', ''
+        else:
+            return '', 404, '', '', wor
+
+def homepage(short_url, status_code, qr_file_path, alias, word):
+    """Renders homepage"""
+    return render_template('homepage.html', url=short_url,
+                           status=status_code, qr_image=qr_file_path,
+                           alias_status=alias, word=word,
+                           cache_id=uuid.uuid4())
+
+def func_alias(ezy_instance, user_output):
+    """Handles:
+    ALIAS EXIST ORIGINAL URL INVALID
+                |
+    Return: (str) "alias exist original url invalid"
+    ALIAS EXIST ORIGINAL URL VALID
+                |
+    Return: (str) "alias exist original url valid"
+
+    ALIAS DOESN'T EXIST ORIGINAL URL INVALID
+                |
+    Return: (str) "alias doesn't exist original url invalid"
+    ALIAS DOESN'T EXIST ORIGINAL URL VALID
+                |
+    Return: (str) "alias doesn't exist original url valid"
+    """
+    alias = ezy_instance.exists(user_output)
+    valid_url = ezy_instance.url()
+
+    if alias and not valid_url:
+        return "alias exist original url invalid"
+    if alias and valid_url:
+        return "alias exist original url valid"
+
+    if not alias and not valid_url:
+        return "alias doesn't exist original url invalid"
+    if not alias and valid_url:
+        return "alias doesn't exist original url valid"
+
+def dashpage(short_url, status_code, qr_file_path, alias, word):
+    """Renders dashpage"""
+    user = User().exists(None, None, session.get('user_id'))
+    if user:
+        info = DBStorage().fetch_user(session.get('user_id'))
+        names = info.first_name + ' ' + info.last_name
+        email = info.email[:2].upper()
+
+    return render_template('dashboard.html', url=short_url,
+                           status=status_code, qr_image=qr_file_path,
+                           alias_status=alias, word=word,
+                           cache_id=uuid.uuid4(), user_id=session['user_id'],
+                           names=names, email=email)
+
+def historypage(user_id, query=None):
+    """Renders the history page returns crucial info"""
+    if query:
+        long_result = DBStorage().search(user_id, query)
+
+        if len(query) > 10:
+            query = query[-7:]
+            short_result = DBStorage().search(user_id, None, query)
+
+        result = long_result if long_result else short_result
+
+        user = User().exists(None, None, user_id)
+        info = DBStorage().fetch_user(user_id)
+        names = info.first_name + ' ' + info.last_name
+        email = info.email[:2].upper()
+
+        if result:
+            sear = []
+            for obj in result:
+                cl_name = type(obj).__name__
+                id = obj.id
+                attr = {k: v for k, v in obj.__dict__.items()
+                        if not k.startswith('_sa_instance_state')}
+            
+                sear.append({
+                    **attr
+                })
+            search_result = sorted(sear, key=lambda x: x['created_at'],
+                                   reverse=True)
+            # search result successful
+            return render_template('user_routes/history.html',
+                                   cache_id=uuid.uuid4(),
+                                   email=email, names=names,
+                                   user_id=user_id,
+                                   history_items=search_result)
+        else:
+            return render_template('user_routes/history.html',
+                                   cache_id=uuid.uuid4(),
+                                   email=email, names=names,
+                                   user_id=user_id,
+                                   info="NO RESULTS")
+
+    user = User().exists(None, None, user_id)
+    if user:
+        info = DBStorage().fetch_user(user_id)
+        names = info.first_name + ' ' + info.last_name
+        email = info.email[:2].upper()
+        his = DBStorage().fetch_user_and_ezy(user_id)
+        history = sorted(his, key=lambda x: x['created_at'],
+                         reverse=True)
+        return render_template('user_routes/history.html',
+                               cache_id=uuid.uuid4(),
+                               email=email, names=names,
+                               user_id=user_id,
+                               history_items=history)
+    else:
+        # direct a user if they've been blocked to sign in
+        session['info_message'] = "Account doesn't exist"
+        return redirect(url_for('web_app.sign_in'))
 
 @web_app_blueprint.route('/logout', methods=["GET"])
 def logout():
