56d8d5f4f2a77ae4b597056ebeb714aeb6c5e689
Bigizic <olalekanisaac75@gmail.com>
2023-12-10 12:18:05 +0000

    Update users.py

--- v2/models/users.py
+++ v2/models/users.py
@@ -33,3 +33,24 @@ class User(EzyModel, Base):
             super().__setattr__(attribute, hashed_password)
         else:
             super().__setattr__(attribute, value)
+
+    def edit_details(self, user_id, DBStorage_ins, names=None, passW=None):
+        """ Edit a userr's credential """
+        # check for existence of user via email return id and password
+        user = DBStorage_ins.fetch_user(user_id)
+        if user:
+            old_pass = user.password
+
+        if names:
+                DBStorage_ins.update_user(user_id, names[0], names[1], None)
+                return True
+
+        if passW:
+            user_pass = passW[0]  # user entered password from browser
+            compare_old = bcrypt.checkpw(old_pass.encode(), user_pass.encode())
+            if compare_old:
+                # replace old password with new one 
+                DBStorage_ins.update_user(user_id, None, None, passW[1])
+                return True
+
+        return False
