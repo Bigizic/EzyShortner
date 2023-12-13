ac5a14cbcf6c8a2b52f0f202ba94dcce702fcf2c
Bigizic <olalekanisaac75@gmail.com>
2023-12-13 22:20:05 +0000

    Update web_app.py

--- v2/app/web_app.py
+++ v2/app/web_app.py
@@ -11,22 +11,30 @@ from shortner.qr_img_gen import qr_gen
 import uuid
 from app.web_app_routes import web_app_blueprint
 import logging
+import random
 
 
 # app = Flask(__name__, static_folder='static', static_url_path='/static')
 app = Flask(__name__)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)  # set session to 7 days
+# set session to 7 days
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
 app.secret_key = environ.get("FLASK_KEY")
 app.url_map.strict_slashes = False
 
 if not app.debug:
     app.logger.setLevel(logging.INFO)
 
+"""def add_random(num):
+    return num + random.randint(1, 10)"""
+
+# app.jinja_env.globals.update(add_random=add_random)
+
 
 @app.before_request
 def before_request():
     """ Initialize logged_in status in session """
     session.setdefault('logged_in', False)
+
 
 app.register_blueprint(web_app_blueprint)
 
