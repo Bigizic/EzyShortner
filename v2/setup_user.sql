e536b4096fc6f19488a8bfb74ea74751093d6b46
Bigizic <olalekanisaac75@gmail.com>
2023-12-21 16:55:09 +0000

    Update setup_user.sql

--- v2/setup_user.sql
+++ v2/setup_user.sql
@@ -4,3 +4,4 @@ CREATE USER IF NOT EXISTS 'ezy_user'@'localhost' IDENTIFIED BY '0000';
 GRANT ALL PRIVILEGES ON `Ezy_url_2`.* TO 'ezy_user'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'ezy_user'@'localhost';
 FLUSH PRIVILEGES;
+ALTER USER 'ezy_user'@'localhost' IDENTIFIED WITH mysql_native_password BY '0000';
