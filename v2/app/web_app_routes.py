da785fecea3b01b8320d49fd9539d9e23ef71253
Bigizic <olalekanisaac75@gmail.com>
2023-12-29 03:17:42 +0000

    Update web_app_routes.py

--- v2/app/web_app_routes.py
+++ v2/app/web_app_routes.py
@@ -31,8 +31,8 @@ import random
 import string
 
 web_app_blueprint = Blueprint('web_app', __name__)
-CLIENT_ID = ('518132922807-8vsde0i71v5nktavtssmt1j8vtugvu6o
-             .apps.googleusercontent.com')
+CLIENT_ID = ('518132922807-8vsde0i71v5nktavtssmt1j8vtugvu6o'
+             '.apps.googleusercontent.com')
 
 
 def check_session():
